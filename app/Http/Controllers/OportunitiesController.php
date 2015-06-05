1f7ca83360e4b3bc6aa6ce40a3d5e82c5f26ffb5
danielsatiro <danielsatiro2003@yahoo.com.br>
2015-06-04 22:38:49 -0300

    adicionado cadastro e atualização de oportunidades

---
+++ app/Http/Controllers/OportunitiesController.php
@@ -0,0 +1,93 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models;
+
+class OportunitiesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return \Response::json('Forbiden Access',403);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$data = \Input::all();
+		$newOportunity = Models\Company::newOportunity($data);
+
+		return \Response::json($newOportunity, $newOportunity['return_code']);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$oportunity = Models\Oportunity::getOportunity($id);
+		return \Response::json($oportunity);
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		return \Response::json('Forbiden Access',403);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		$data = \Input::all()
+		$data['id_oportunities'] = $id;
+
+		$oportunity = Models\Oportunity::updateOportunity($data);
+		return \Response::json($oportunity, $oportunity['return_code']);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
--- app/Models/Oportunity.php
+++ app/Models/Oportunity.php
@@ -6,7 +6,7 @@ class Oportunity extends Model {
 
 	protected $table = 'oportunities';
 	protected $primaryKey = 'id_oportunities';
-	protected $fillable = ['id_users', 'id_companies', 'salary', 'period_salary', 'description', 'begins', 'ends'];
+	protected $fillable = ['id_users', 'id_companies', 'title', 'salary', 'period_salary', 'description', 'begins', 'ends'];
 	protected $hidden = ['deleted_at', 'updated_at', 'created_at'];
 	protected $softDelete = true;
 	public $timestamps = true;
@@ -21,5 +21,85 @@ class Oportunity extends Model {
 
 	public function guests(){
 		return $this->hasMany('App\Models\Guest', 'id_oportunities', 'id_oportunities');
+	}
+
+	/*
+	* Validate oportunity data
+	*
+	* @param array $data Data of oportunity
+	* @param String $type Type of validation Create or Update
+	*
+	* @return Illuminate\Validation\Validator
+	*/
+	public static validateOportunity($data, $type = 'C'){
+		$rules = array(
+		    'id_users' => 'required|integer|exists:users,id_users',
+		    'id_companies' => 'required|integer|exists:companies,id_companies',
+		    'title' => 'required|regex:/^([a-zà-úÀ-Ú\x20])+$/i',
+		    'salary' => 'numeric',
+		    'period_salary' => 'in:hora,dia,semana,mês,ano',
+		    'description' => 'required',
+		    'begins' => 'required|date_format:Y-d-m H:i:s',
+		    'ends' => 'required|date_format:Y-d-m H:i:s'
+		);
+
+		if ($type == 'U') :
+			$rules['id_oportunities'] = 'required|integer|exists:oportunities,id_oportunities';
+		endif;
+
+		return \Validator::make($data, $rules);
+	}
+
+	public static function newOportunity($data){
+		$validate = self::validateOportunity($data, 'C');
+ 		
+		//TODO validar Acl
+
+	    if ($validate->fails()){
+			$response['messages'] = $validate->messages()->toArray();
+			$response['return_code'] = 406;
+			return $response;
+	    }
+
+		$oportunity = new self;
+	 	
+	 	$oportunity->fill($data);
+	    $oportunity->save();
+	 
+	    return ['oportunity' => $oportunity, 'return_code' => 201];
+	}
+
+	public static function updateOportunity($data){
+		$validate = self::validateOportunity($data, 'U');
+ 		
+		//TODO validar Acl
+ 
+	    if ($validate->fails()){
+			$response['messages'] = $validate->messages()->toArray();
+			$response['return_code'] = 406;
+			return $response;
+	    }
+
+	    $oportunity = self::find($data['id_oportunities']);
+	    $oportunity->fill($data);
+	    $oportunity->save();
+
+	    return ['oportunity' => $oportunity, 'return_code' => 200];
+	}
+
+	public static function getOportunity($idOportunity){
+		return self::find($idOportunity);
+	}
+
+	/*
+	* Delete a Oportunity
+	*
+	* @param int $idOportunity ID of oportunity
+	*
+	* @return boolean
+	*/
+	public static function deleteCompany($idOportunity){
+		$oportunity = self::find($idOportunity);
+		return $oportunity->delete();
 	}
 }
